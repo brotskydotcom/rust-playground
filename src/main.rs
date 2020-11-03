8237fd83b6325c440ef1d8dbdefcdb34450fd3ad
Daniel Brotsky <dev@brotsky.com>
2020-11-02 19:29:26 -0800

    serde_json tests

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,21 @@
-use sqlx::sqlite::{SqliteQueryAs, SqlitePool};
+use serde_json::{Value, from_str, to_string};
 
-const DB_NAME: &str = "local/test.db";
-const REQUEST_SCHEMA: &str =
-    r#"create table if not exists requests (
-        id integer primary key,
-        timestamp integer not null,
-        cache_key text not null unique,
-        body text not null
-    )"#;
-
-#[derive(Debug, sqlx::FromRow)]
-struct Request {
-    id: i64,
-    timestamp: i64,
-    cache_key: String,
-    body: String,
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let body: Value = from_str(r#"
+        {"a": "a", "b": {"c": "c", "d": "d"}, "e": "e"}
+        "#)?;
+    println!("a: {}", string_or(&body["a"], "not a"));
+    println!("bc: {}", string_or(&body["b"]["c"], "not bc"));
+    println!("bd: {}", string_or(&body["b"]["d"], "not bc"));
+    println!("ea: {}", string_or(&body["e"]["a"], "not ea"));
+    println!("fa: {}", string_or(&body["f"]["a"], "not fa"));
+    Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let pool = SqlitePool::builder()
-        .max_size(5)
-        .build(format!("sqlite:{}", DB_NAME).as_str()).await?;
-    sqlx::query(REQUEST_SCHEMA).execute(&pool).await?;
-    sqlx::query("replace into requests (timestamp, cache_key, body) values (?, ?, ?)")
-        .bind(4i64).bind("key1").bind("body1")
-        .execute(&pool)
-        .await?;
-    sqlx::query("replace into requests (timestamp, cache_key, body) values (?, ?, ?)")
-        .bind(5i64).bind("key2").bind("body2")
-        .execute(&pool)
-        .await?;
-    sqlx::query("replace into requests (timestamp, cache_key, body) values (?, ?, ?)")
-        .bind(6i64).bind("key3").bind("body3")
-        .execute(&pool)
-        .await?;
-    let result: (i64,) = sqlx::query_as("select last_insert_rowid()")
-        .fetch_one(&pool)
-        .await?;
-    println!("{:?}", result);
-    let result: Request = sqlx::query_as("select * from requests where cache_key = ?")
-        .bind("key1")
-        .fetch_one(&pool)
-        .await?;
-    println!("{:?}", result);
-    Ok(())
+fn string_or(v: &Value, default: &str) -> String {
+    if v.is_string() {
+        to_string(v).unwrap()
+    } else {
+        default.to_string()
+    }
 }
