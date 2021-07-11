8eb97234fdda7cd1a45be2ec4135b42eda61cd6d
Daniel Brotsky <dev@brotsky.com>
2021-07-11 07:55:05 -0700

    test PartialEq of structures (with Box).

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-extern crate tokio; // 1.7.1;
+#[derive(PartialEq, Debug, Clone, Default)]
+struct A {
+    a1: String,
+    a2: Option<Box<B>>,
+}
 
-use hyper::Client;
-use std::time::Duration;
+#[derive(PartialEq, Debug, Clone, Default)]
+struct B {
+    b1: String,
+    b2: Option<Box<A>>,
+}
 
-#[tokio::main]
-async fn main() {
-    let client = Client::new();
-
-    let uri = "http://stackoverflow.com".parse().unwrap();
-    let work = client.get(uri);
-
-    match tokio::time::timeout(Duration::from_millis(10), work).await {
-        Ok(result) => match result {
-            Ok(response) => println!("Status: {}", response.status()),
-            Err(e) => println!("Network error: {:?}", e),
-        },
-        Err(_) => println!("Timeout: no response in 10 milliseconds."),
-    };
+fn main() {
+    let mut a1 = A::default();
+    let mut a2 = A::default();
+    let mut b1 = B::default();
+    let b2 = B::default();
+    assert_eq!(a1, a2, "default A values are not equal");
+    assert_eq!(b1, b2, "default B values are not equal");
+    b1.b1 = String::new();
+    assert_eq!(b1, b2, "new empty Strings are not equal");
+    a1.a2 = Some(Box::new(b1.clone()));
+    a2.a2 = Some(Box::new(b1.clone()));
+    assert_eq!(a1, a2, "pointer to same val in a2 is not equal");
+    let mut b2= b2.clone();
+    b2.b2 = Some(Box::new(a1.clone()));
+    a2.a2 = Some(Box::new(b2));
+    assert_ne!(a1, a2, "deep equality is not being checked")
 }
