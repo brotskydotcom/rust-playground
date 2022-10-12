5f1ffe9478794a9fd603c599f55f06a397da1b5c
Daniel Brotsky <dev@brotsky.com>
2022-10-11 22:58:32 -0700

    test time parsing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
-#[derive(PartialEq, Debug, Clone, Default)]
-struct A {
-    a1: String,
-    a2: Option<Box<B>>,
-}
-
-#[derive(PartialEq, Debug, Clone, Default)]
-struct B {
-    b1: String,
-    b2: Option<Box<A>>,
-}
+use chrono::{DateTime, Utc};
 
 fn main() {
-    let mut a1 = A::default();
-    let mut a2 = A::default();
-    let mut b1 = B::default();
-    let b2 = B::default();
-    assert_eq!(a1, a2, "default A values are not equal");
-    assert_eq!(b1, b2, "default B values are not equal");
-    b1.b1 = String::new();
-    assert_eq!(b1, b2, "new empty Strings are not equal");
-    a1.a2 = Some(Box::new(b1.clone()));
-    a2.a2 = Some(Box::new(b1.clone()));
-    assert_eq!(a1, a2, "pointer to same val in a2 is not equal");
-    let mut b2= b2.clone();
-    b2.b2 = Some(Box::new(a1.clone()));
-    a2.a2 = Some(Box::new(b2));
-    assert_ne!(a1, a2, "deep equality is not being checked")
+    for s in [
+        "1970-01-01T00:00:00.030Z",
+        "1970-01-01T00:00:00.030+0000",
+        "1970-01-01T00:00:00.030+00:00",
+    ] {
+        if let Ok(dt) = DateTime::parse_from_str(s, "%+") {
+            println!("Parsed epoch millis are: {}", dt.timestamp_millis())
+        } else {
+            eprintln!("Couldn't parse {s} with %+")
+        }
+        if let Ok(dt) = s.parse::<DateTime<Utc>>() {
+            println!("Parsed epoch millis are: {}", dt.timestamp_millis())
+        } else {
+            eprintln!("Couldn't parse {s} as DateTime<Utc>")
+        }
+    }
 }
