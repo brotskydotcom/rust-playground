21bb0052e2e192b401e07b0c9cd8913b10819fc0
Daniel Brotsky <dev@brotsky.com>
2022-11-04 16:34:44 -0700

    test warp rejection responses

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
-use chrono::{DateTime, Utc};
+use warp::Filter;
 
-fn main() {
-    for s in [
-        "1970-01-01T00:00:00.030Z",
-        "1970-01-01T00:00:00.030+0000",
-        "1970-01-01T00:00:00.030+00:00",
-    ] {
-        if let Ok(dt) = DateTime::parse_from_str(s, "%+") {
-            println!("Parsed epoch millis are: {}", dt.timestamp_millis())
-        } else {
-            eprintln!("Couldn't parse {s} with %+")
-        }
-        if let Ok(dt) = s.parse::<DateTime<Utc>>() {
-            println!("Parsed epoch millis are: {}", dt.timestamp_millis())
-        } else {
-            eprintln!("Couldn't parse {s} as DateTime<Utc>")
-        }
-    }
+#[tokio::main]
+async fn main() {
+    let route1 = warp::any()
+        .and(warp::header::<String>("X-Frotz"))
+        .map(|ct| format!("Accepted Request with X-Frotz: {}\r\n\r\n", &ct));
+    warp::serve(route1).run(([127, 0, 0, 1], 3030)).await;
 }
