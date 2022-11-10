e1797c7b5b855ba1d00e7e1f286f242893337f10
Daniel Brotsky <dev@brotsky.com>
2022-11-10 11:04:46 -0800

    test parsing/conversion of IP and socket addresses

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
-use warp::Filter;
-
-type QueryParams = std::collections::HashMap<String, String>;
-
 #[tokio::main]
 async fn main() {
-    let qf = warp::get().and(warp::query::<QueryParams>()).map(|q| {
-        eprintln!("Parsed: {:?}", q);
-        format!("Parsed: {:?}", q)
-    });
-    let rqf = warp::post().and(warp::filters::query::raw()).map(|q| {
-        eprintln!("Raw: {}", q);
-        format!("Raw: {}", q)
-    });
-    let handle = tokio::spawn(warp::serve(qf.or(rqf)).run(([127, 0, 0, 1], 3030)));
-    let client = reqwest::Client::builder().build().unwrap();
-    let request = client
-        .get("http://localhost:3030/query?this=is+a+test")
-        .build()
-        .unwrap();
-    let response = client.execute(request).await.unwrap();
-    eprintln!("Response: {:?}", response);
-    handle.abort();
+    for source_ip in ["127.0.0.1", "127.0.0.1:8080", ":8080"] {
+        let result1 = source_ip.parse::<std::net::SocketAddr>();
+        let result2 = source_ip.parse::<std::net::IpAddr>();
+        let result3 = match result2 {
+            Ok(addr) => Some(std::net::SocketAddr::from((addr, 0))),
+            _ => None,
+        };
+        eprintln!("{}: {:?}, {:?}, {:?}", source_ip, result1, result2, result3);
+    }
 }
