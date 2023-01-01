1b718934a4e72dbe0aa138a440407108f32843fb
Daniel Brotsky <dev@brotsky.com>
2022-12-31 22:25:05 -0800

    list files in huge directory

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-#[tokio::main]
-async fn main() {
-    for source_ip in ["127.0.0.1", "127.0.0.1:8080", ":8080"] {
-        let result1 = source_ip.parse::<std::net::SocketAddr>();
-        let result2 = source_ip.parse::<std::net::IpAddr>();
-        let result3 = match result2 {
-            Ok(addr) => Some(std::net::SocketAddr::from((addr, 0))),
-            _ => None,
-        };
-        eprintln!("{}: {:?}, {:?}, {:?}", source_ip, result1, result2, result3);
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+
+const NGL_DIR: &str = "/Users/dbrotsky/Library/Logs/NGL";
+const OUT_FILE: &str = "/tmp/ngl-list.txt";
+
+fn main() {
+    let result = fs::metadata(NGL_DIR).unwrap();
+    println!("Directory length is: {}MB", result.len() / (1024 * 1024));
+    let mut buf = File::create(OUT_FILE).unwrap();
+    for (i, file) in fs::read_dir(NGL_DIR).unwrap().enumerate() {
+        let file = file.unwrap();
+        writeln!(
+            &mut buf,
+            "{}: {} ({}MB)",
+            i,
+            file.file_name().to_str().unwrap(),
+            file.metadata().unwrap().len(),
+        )
+        .unwrap();
+        if i > 0 && i % 100 == 0 {
+            print!(".")
+        }
+        if i % 8_000 == 0 {
+            println!("{}: ", i)
+        }
     }
 }
