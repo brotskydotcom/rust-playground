654f3db3519d0d844aff487903ad16924a43f559
Daniel Brotsky <dev@brotsky.com>
2023-01-03 23:48:32 -0800

    list files in huge directory (with outfile v2)

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
+use chrono::{DateTime, SecondsFormat, Utc};
 use std::fs::File;
 use std::io::Write;
 use std::{fs, io};
 
-const NGL_DIR: &str = "/Users/dbrotsky/Downloads/NGL";
-const OUT_FILE: &str = "/Users/dbrotsky/Downloads/NGL-list.csv";
+const NGL_DIR: &str = "/Users/dbrotsky/tmp/NGL";
+const OUT_FILE: &str = "/Users/dbrotsky/tmp/NGL-list.csv";
 
 fn main() {
     let metadata = fs::metadata(NGL_DIR).unwrap();
     println!("Directory length is: {}MB", metadata.len() / (1024 * 1024));
     let mut buf = File::create(OUT_FILE).unwrap();
-    writeln!(buf, "Index,Filename,Size (KB)").unwrap();
+    writeln!(buf, "Index,Filename,Size,ModDate").unwrap();
     let mut count = 0;
     for result in fs::read_dir(NGL_DIR).unwrap() {
         let file = result.unwrap();
-        writeln!(
-            &mut buf,
-            "{},{},{}",
-            count,
-            file.file_name().to_str().unwrap(),
-            file.metadata().unwrap().len() / 1024,
-        )
-        .unwrap();
+        let name = file.file_name().to_string_lossy().to_string();
+        let meta = file.metadata().unwrap();
+        let mod_time: DateTime<Utc> = meta.modified().unwrap().into();
+        let mod_time = mod_time.to_rfc3339_opts(SecondsFormat::Secs, true);
+        writeln!(&mut buf, "{},{},{},{}", count, name, meta.len(), mod_time).unwrap();
         count += 1;
         if count % 100 == 0 {
             print!(".");
